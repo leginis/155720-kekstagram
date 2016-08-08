1754204b62eb1822700d8ac981d08882ea29ba5a
Julia <jul@dir01.org>
2016-08-09 01:43:59 +0300

    Added image dimensions to Resizer

--- build/js/resizer.js
+++ build/js/resizer.js
@@ -123,6 +123,15 @@
       this._ctx.rect(-this._container.width / 2, -this._container.height / 2, this._container.width, this._container.height);
       this._ctx.rect(resizeConstraintX, resizeConstraintY, resizeConstraintSide, resizeConstraintSide);
       this._ctx.fill('evenodd');
+
+      this._ctx.textAlign = 'center';
+      this._ctx.fillStyle = 'white';
+      this._ctx.font = 'normal 13px Arial';
+      this._ctx.fillText(
+        this._image.naturalWidth + 'x' + this._image.naturalHeight,
+        0, -this._resizeConstraint.side / 2 - 10
+      );
+
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
